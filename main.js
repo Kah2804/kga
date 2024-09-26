0dacbf8418f092b1102e2c8676cd8600abaaef09
Kah2804 <kalliani.lima@escola.pr.gov.br>
2024-09-26 11:59:01 -0300

    main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function soma (num1,num2){
     return num1 + num2; 
 }
 console.log (soma (1,2));
-function areaQuadrado (lado){
+function areaQuadrado (lado1){
     return lado**2;
 }
 console.log (soma(1,4));
