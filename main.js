edd77f4b2556f59fdeae27b594a59d9513f1f804
Kah2804 <kalliani.lima@escola.pr.gov.br>
2024-09-26 12:09:35 -0300

    main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-alert("kalliani 19 giovanna 41")
-let area=parseInt (prompt(area))
-document.write(area)
+alert("kalliani 19 giovanna 41");
+let area=parseInt (prompt(area));
+document.write(area);
 function areaQuadrado (lado){
     return lado**2;
 }
-console.log(areaQuadrado(area))
+console.log(areaQuadrado(area));
